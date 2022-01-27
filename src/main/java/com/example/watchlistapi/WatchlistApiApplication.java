f2176e8f0557cb0b29d50a86ea98af1a63ffa44e
Xander Nguyen <xander.nguyen313@gmail.com>
2022-01-27 14:13:36 -0600

    Initial commit

---
+++ src/main/java/com/example/watchlistapi/WatchlistApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.watchlistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchlistApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchlistApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/watchlistapi/WatchlistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.watchlistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchlistApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
