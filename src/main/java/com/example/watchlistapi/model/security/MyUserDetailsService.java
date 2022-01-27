fde3075fe557c41742790ad6bcbe1e44e91c2ce7
Xander Nguyen <xander.nguyen313@gmail.com>
2022-01-27 14:23:28 -0600

    Implement password encoder and JWT and authorization

---
+++ src/main/java/com/example/watchlistapi/model/security/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.watchlistapi.model.security;
+
+
+import com.example.watchlistapi.model.User;
+import com.example.watchlistapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findUserByEmailAddress(email);
+        return new MyUserDetails(user);
+    }
+}
