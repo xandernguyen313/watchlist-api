9d7d656386522334322cdd43562f6caf79dab782
Xander Nguyen <xander.nguyen313@gmail.com>
2022-01-31 13:56:55 -0600

    Update application.properties to deploy on heroku

--- src/main/java/com/example/watchlistapi/service/AppService.java
+++ src/main/java/com/example/watchlistapi/service/AppService.java
@@ -12,11 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
+import java.util.List;
 @Service
 public class AppService {
     private SymbolRepository symbolRepository;
